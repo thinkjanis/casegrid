8934164db669807ad4eddb157dd997d873b44541
Janis <31632325+thinkjanis@users.noreply.github.com>
2025-03-03 21:11:03 +0200

    Added subnet

--- main.tf
+++ main.tf
@@ -23,3 +23,12 @@ resource "aws_vpc" "casegrid_vpc" {
   }
 }
 
+# Subnet
+resource "aws_subnet" "casegrid_subnet" {
+    vpc_id = aws_vpc.casegrid_vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "eu-west-2a"
+    tags = {
+      Name = "casegrid_subnet"
+    }
+}
