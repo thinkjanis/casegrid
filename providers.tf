df8f42a066f3e12854c05fa7eaa1237f2584a003
Janis <31632325+thinkjanis@users.noreply.github.com>
2025-03-06 00:38:36 +0200

    Seperate providers file

---
+++ providers.tf
@@ -0,0 +1,31 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# PROVIDERS
+# ---------------------------------------------------------------------------------------------------------------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "thinkjanis"
+
+    workspaces {
+      name = "casegrid"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
